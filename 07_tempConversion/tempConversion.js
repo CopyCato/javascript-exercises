c5e1be8be20a81322b158c5d46919c29aed7d308
CopyCato <nikola93nikolov@gmail.com>
2022-05-24 14:37:20 +0200

    Add exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahr) {
+  let cels = (fahr - 32) * (5/9);
+  let rounded = Math.round(cels * 10) / 10;
+  return rounded;
 };
 
-const ctof = function() {
-
+const ctof = function(cels) {
+  let fahr = cels * (9/5) + 32;
+  let rounded = Math.round(fahr * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
