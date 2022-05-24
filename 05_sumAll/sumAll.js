f4fa3a42343cac1734979cde1aeb0da4dc3462f2
CopyCato <nikola93nikolov@gmail.com>
2022-05-24 14:13:57 +0200

    Add exercise 06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+  sum = 0;
+  if(x < 0 || y < 0)
+    return 'ERROR';
+  if(!Number.isInteger(x) || !Number.isInteger(y)) 
+    return "ERROR";
+  if(x > y) {
+    for(let i = y; i <= x; i++){
+      sum += i;
+    }
+    return sum;
+  }else
+  for(let i = x; i <= y; i++){
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
